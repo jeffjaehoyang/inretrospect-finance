a6bac3603696c3a4fd4f7df765c75780afff6ef3
Jeff Yang <jho.yang96@gmail.com>
2021-12-12 23:07:14 +0900

    added tailwind.output.css to gitignore

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -4,7 +4,6 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        "light-blue": colors.lightBlue,
         cyan: colors.cyan,
         warmGray: colors.warmGray,
         emerald: colors.emerald,
