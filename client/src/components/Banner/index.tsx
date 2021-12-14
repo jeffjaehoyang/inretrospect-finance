5599a5862cf60054432f1cd44d6e4a5cbcf7b33d
Jeff Yang <jho.yang96@gmail.com>
2021-12-14 14:15:12 +0900

    minor

--- client/src/components/Banner/index.tsx
+++ client/src/components/Banner/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { GrMoney } from 'react-icons/gr';
 import { MdCheckCircleOutline, MdDoNotDisturbAlt } from 'react-icons/md';
+import { VscTriangleDown, VscTriangleUp } from 'react-icons/vsc';
 
 import { useFirebaseAuth } from '../../auth/FirebaseAuthContext';
 import * as Styled from './styles';
@@ -26,30 +27,32 @@ const Banner: React.FC<Props> = ({
         </span>
         <div className="flex flex-col ml-4">
           <div className="flex items-center font-bold">
-            <MdCheckCircleOutline className="mr-2 text-lg text-green-800" />
-            Yes: &nbsp;
-            {investedBalance >= 0
-              ? `$${investedBalance.toLocaleString("en-US", {
-                  minimumFractionDigits: 2,
-                  maximumFractionDigits: 2,
-                })}`
-              : `-$${Math.abs(investedBalance).toLocaleString("en-US", {
-                  minimumFractionDigits: 2,
-                  maximumFractionDigits: 2,
-                })}`}
+            <MdCheckCircleOutline className="mr-2 text-lg" />
+            Yes → &nbsp;
+            {investedBalance >= 0 ? (
+              <VscTriangleUp className="text-green-800" />
+            ) : (
+              <VscTriangleDown className="text-red-800" />
+            )}
+            $
+            {Math.abs(investedBalance).toLocaleString("en-US", {
+              minimumFractionDigits: 2,
+              maximumFractionDigits: 2,
+            })}
           </div>
           <div className="flex items-center font-bold">
-            <MdDoNotDisturbAlt className="mr-2 text-lg text-red-800" />
-            No: &nbsp;
-            {notInvestedBalance >= 0
-              ? `$${notInvestedBalance.toLocaleString("en-US", {
-                  minimumFractionDigits: 2,
-                  maximumFractionDigits: 2,
-                })}`
-              : `-$${Math.abs(investedBalance).toLocaleString("en-US", {
-                  minimumFractionDigits: 2,
-                  maximumFractionDigits: 2,
-                })}`}
+            <MdDoNotDisturbAlt className="mr-2 text-lg" />
+            No → &nbsp;
+            {notInvestedBalance >= 0 ? (
+              <VscTriangleUp className="text-green-800" />
+            ) : (
+              <VscTriangleDown className="text-red-800" />
+            )}
+            $
+            {Math.abs(notInvestedBalance).toLocaleString("en-US", {
+              minimumFractionDigits: 2,
+              maximumFractionDigits: 2,
+            })}
           </div>
         </div>
       </Styled.BalanceWrapper>
